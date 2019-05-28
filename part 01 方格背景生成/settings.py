694c940b6dc15719cb7c5afe180037bec468b8f8
typeme <typeme3@163.com>
2019-05-28 13:23:04 +0800

    part 1

---
+++ part 01 方格背景生成/settings.py
@@ -0,0 +1,19 @@
+# 定义了一些颜色 (R, G, B)
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+DARKGREY = (40, 40, 40)
+LIGHTGREY = (100, 100, 100)
+GREEN = (0, 255, 0)
+RED = (255, 0, 0)
+YELLOW = (255, 255, 0)
+
+# 游戏基本设定 
+WIDTH = 1024   # 16 * 64 or 32 * 32 or 64 * 16
+HEIGHT = 768  # 16 * 48 or 32 * 24 or 64 * 12
+FPS = 60 # 刷新率
+TITLE = "Tilemap Demo"
+BGCOLOR = DARKGREY # 背景颜色
+
+TILESIZE = 32 # 方格的尺寸
+GRIDWIDTH = WIDTH / TILESIZE  # 每行方格的数量
+GRIDHEIGHT = HEIGHT / TILESIZE # 每列方格的数量
