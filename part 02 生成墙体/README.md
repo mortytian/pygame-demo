e7144062b85c9ae47566d8a94144c260f0fe8e22
typeme <typeme3@163.com>
2019-05-29 14:00:07 +0800

    part 2

---
+++ part 02 生成墙体/README.md
@@ -0,0 +1,35 @@
+# Pygame-demo 02
+part 02 新增 
+
+墙不能通过
+
+根据txt内容生成墙
+
+
+
+![part 02 demo](https://github.com/typeme/pygame-demo/blob/master/pictute/part%2002%20demo.png)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ part 02 生成墙体/main.py
@@ -0,0 +1,93 @@
+import pygame as pg
+import sys
+from settings import *
+from sprites import *
+from os import path
+
+class Game:
+    def __init__(self):
+        pg.init()
+        self.screen = pg.display.set_mode((WIDTH, HEIGHT))
+        pg.display.set_caption(TITLE)
+        self.clock = pg.time.Clock()
+        pg.key.set_repeat(500, 100) # 开启持续按键
+        self.load_data()
+        
+
+    def load_data(self):
+        game_folder = path.dirname(__file__)
+        self.map_data = []
+        with open(path.join(game_folder,'map.txt'),'rt') as f:
+            for line in f:
+                self.map_data.append(line)
+
+    def new(self):
+        # 初始化
+        self.all_sprites = pg.sprite.Group()
+        self.walls = pg.sprite.Group()
+        for row, tiles in enumerate(self.map_data):
+            for col, tile in enumerate(tiles):
+                if tile == '1':
+                    Wall(self,col,row)
+                # 生成player同时防止生成的player在wall里面
+                if tile == 'P':
+                    self.player = Player(self, col, row)
+
+
+    def run(self):
+        # 为True则game loop 一直运行
+        self.playing = True
+        while self.playing:
+            self.dt = self.clock.tick(FPS) / 1000
+            self.events()
+            self.update()
+            self.draw()
+
+    def quit(self):
+        pg.quit()
+        sys.exit()
+
+    def update(self):
+        # update portion of the game loop
+        self.all_sprites.update()
+
+    def draw_grid(self):
+        # 画出横向方格
+        for x in range(0, WIDTH, TILESIZE):
+            pg.draw.line(self.screen, LIGHTGREY, (x, 0), (x, HEIGHT))
+        # 画出纵向方格
+        for y in range(0, HEIGHT, TILESIZE):
+            pg.draw.line(self.screen, LIGHTGREY, (0, y), (WIDTH, y))
+
+    def draw(self):
+        self.screen.fill(BGCOLOR)
+        self.draw_grid()
+        self.all_sprites.draw(self.screen)
+        pg.display.flip()
+
+    def events(self):
+        # 捕捉键盘事件
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                self.quit()
+            if event.type == pg.KEYDOWN:
+                if event.key == pg.K_ESCAPE:
+                    self.quit()
+                if event.key == pg.K_LEFT:
+                    self.player.move(dx=-1)
+                if event.key == pg.K_RIGHT:
+                    self.player.move(dx=1)
+                if event.key == pg.K_UP:
+                    self.player.move(dy=-1)
+                if event.key == pg.K_DOWN:
+                    self.player.move(dy=1)
+
+    def show_start_screen(self):
+        pass
+# 开始
+g = Game()
+g.show_start_screen()
+while True:
+    g.new()
+    g.run()
+    g.show_go_screen()
